d4ddb4df8db389a6c39571209455385a2ac77fc0
bogdansolomich <bogdansolomich@gmail.com>
2026-06-08 20:01:56 +0300

    WIP: save progress before updating main

--- src/store/api.ts
+++ src/store/api.ts
@@ -2,14 +2,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ 
+  baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
     prepareHeaders: (headers) => {
-      const token = localStorage.getItem('accessToken'); 
+      const token = localStorage.getItem('accessToken');
       if (token) headers.set('authorization', `Bearer ${token}`);
       return headers;
     },
   }),
   // Изначально эндпоинты пустые!
-  endpoints: () => ({}), 
+  endpoints: () => ({}),
 });
